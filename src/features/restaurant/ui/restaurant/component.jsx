f728a71cc3d71cbf883346751424c0d3d7d14c0e
Ivan Glebov <iglebov@kameleoon.com>
2022-04-16 10:34:44 +0300

    dz1

--- src/features/restaurant/ui/restaurant/component.jsx
+++ src/features/restaurant/ui/restaurant/component.jsx
@@ -1,10 +1,11 @@
 import { Menu } from "../../../menu/ui/menu/component";
-
+import { Reviews } from "../../../review/ui/reviews/component";
 export const Restaurant = ({ restaurant }) => {
   return (
     <div>
       <span>{restaurant.name}</span>
       <Menu menu={restaurant.menu} />
+      <Reviews reviews={restaurant.reviews}/>
     </div>
   );
 };
