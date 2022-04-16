440abe8e835fb0f45e5e3f32cf3d47ec63d39d8b
brinevdev <theorion2015@gmail.com>
2022-04-16 18:51:05 +0300

    add key for reviews

--- src/features/review/ui/reviews/component.jsx
+++ src/features/review/ui/reviews/component.jsx
@@ -2,6 +2,6 @@ import { Review } from "../review/component"
 
 export const Reviews = ({reviews}) => {
    return reviews.map((review) => {
-    return <Review review={review}/>
+    return <Review key = {review.id} review={review}/>
    });
 }
