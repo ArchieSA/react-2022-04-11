633013a6eb8359f31eb08b76651b47dbb5d8139e
natallia.bahuk <natallia.bahuk@itechart-group.com>
2022-04-17 16:10:33 +0300

    HomeTask 1 from Natallia Bahuk

--- src/features/restaurant/ui/restaurant/component.jsx
+++ src/features/restaurant/ui/restaurant/component.jsx
@@ -1,10 +1,20 @@
 import { Menu } from "../../../menu/ui/menu/component";
+import { Rate } from "../../../rate/ui/rate/component";
+import { Reviews } from "../../../review/ui/reviews/component";
 
 export const Restaurant = ({ restaurant }) => {
+  const sumRating = restaurant.reviews.reduce(
+    (sum, item) => sum + item.rating,
+    0
+  );
+  const rating = sumRating / restaurant.reviews.length;
+
   return (
     <div>
       <span>{restaurant.name}</span>
+      <Rate value={rating} />
       <Menu menu={restaurant.menu} />
+      <Reviews reviews={restaurant.reviews} />
     </div>
   );
 };
