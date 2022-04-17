d045f2e6a1676f606306b41ee37a3c3a75320126
Vildan Rezepov <llapotkovs@yandex.ru>
2022-04-17 21:36:08 +0300

    Home Task 1

--- src/features/restaurant/ui/restaurant/component.jsx
+++ src/features/restaurant/ui/restaurant/component.jsx
@@ -1,10 +1,16 @@
 import { Menu } from "../../../menu/ui/menu/component";
+import {Rate} from "../../../rate/ui/rate/component";
+import {Reviews} from "../../../review/ui/reviews/component";
 
 export const Restaurant = ({ restaurant }) => {
+  const rating = (restaurant.reviews.reduce((sum, current) => sum + current.rating, 0)) / restaurant.reviews.length
+
   return (
     <div>
       <span>{restaurant.name}</span>
       <Menu menu={restaurant.menu} />
+      <Rate value={rating} />
+      <Reviews reviews={restaurant.reviews} />
     </div>
   );
 };
---
+++ src/features/review/ui/review/component.jsx
@@ -0,0 +1,11 @@
+import {Rate} from "../../../rate/ui/rate/component";
+
+export const Review = ({ review }) => {
+  return(
+    <div>
+      <div>User: {review.user}</div>
+      <div>Text: {review.text}</div>
+      <Rate value={review.rating} />
+    </div>
+  )
+}
