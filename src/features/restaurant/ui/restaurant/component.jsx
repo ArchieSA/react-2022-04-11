9cb0d80114e9e56ae2225562c2e3c22f8543756c
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2022-04-18 20:27:12 +0300

    Add files via upload

--- src/features/restaurant/ui/restaurant/component.jsx
+++ src/features/restaurant/ui/restaurant/component.jsx
@@ -1,10 +1,14 @@
 import { Menu } from "../../../menu/ui/menu/component";
+import { Rate } from "../../../rate/ui/rate/component";
+import { Reviews } from "../../../review/ui/reviews/component";
 
 export const Restaurant = ({ restaurant }) => {
   return (
     <div>
       <span>{restaurant.name}</span>
       <Menu menu={restaurant.menu} />
+      <Rate value={restaurant.rate} />
+      <Reviews reviews={restaurant.reviews} />
     </div>
   );
 };
