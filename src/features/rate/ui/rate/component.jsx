dc36a6ffac2e3675a3b8759632e9c84f7b5ed9be
NikiforowN <kiwiwiwitsk@gmail.com>
2022-04-19 21:32:54 +0700

    homework 2 done

--- src/features/rate/ui/rate/component.jsx
+++ src/features/rate/ui/rate/component.jsx
@@ -1,1 +1,17 @@
-export const Rate = ({ value }) => <span>{value}</span>;
+import {ReactComponent as StarIcon} from './imgs/star.svg';
+import styles from './styles.module.scss';
+
+
+export const Rate = ({ value }) => {
+   const rating = [];
+
+   for (let i = 0; i < 5; i++) {
+     i < value ? rating.push(<StarIcon key={i} className={styles.iconGold}/>) : rating.push(<StarIcon key={i} className={styles.iconGrey}/>)
+   };
+
+   return(
+      <div>
+         {rating}
+      </div>
+   );
+};
--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { Restaurant } from "../restaurant/component";
 
 export const Restaurants = ({ restaurants }) => {
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
-      <Restaurant restaurant={restaurants[0]} />
+      {/* Хотелось бы разбить это на два компонента, но пока не понял, как прокинуть useState */}
+      {restaurants.map((restaurant, index) => (
+        <button key={restaurant.id} onClick={() => setIndex(index)}> {restaurant.name} </button>
+      ))};
+
+      <Restaurant restaurant={restaurants[index]} />
     </div>
   );
 };
