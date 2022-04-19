1a028033bd397660c82e145db3738613532a044c
Alexander Khusnullin <84662349+AleKhuRu@users.noreply.github.com>
2022-04-19 09:11:46 +0300

    Update component.jsx

--- src/features/review/ui/review/component.jsx
+++ src/features/review/ui/review/component.jsx
@@ -1,11 +1,1 @@
-import { Rate } from "../../../rate/ui/rate/component";
 
-export const Review = ({review}) => {
-    return (
-        <div>
-            <div>{review.user}</div>
-            <div>{review.text}</div>
-            <Rate rate={{rate: review.rating}} />
-        </div>
-    );
-}
