37d7fc7942fd3501f66e24900a3516ce70f226cb
brinevdev <theorion2015@gmail.com>
2022-04-19 23:59:00 +0300

    2-task

--- src/features/rate/ui/rate/component.jsx
+++ src/features/rate/ui/rate/component.jsx
@@ -3,7 +3,7 @@ import star from './imgs/star.svg'
 export const Rate = ({ value }) => {
     const rate = [];
     for (let i=0; i<value;i++){
-        rate.push(<img src={star}/>)
+        rate.push(<img key={i} src={star} alt=''/>)
     }
    return <div>{rate}</div>;
 };
--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -1,9 +1,18 @@
 import { Restaurant } from "../restaurant/component";
+import {useState} from 'react';
 
 export const Restaurants = ({ restaurants }) => {
+  const buttonsRestaurant = [];
+  let [index,setIndex] = useState(0);
+  for (let i=0;i<restaurants.length;i++){
+    buttonsRestaurant.push(<button key={restaurants[i].id} onClick={()=>setIndex(i)}>{restaurants[i].name}</button>)
+  }
   return (
     <div>
-      <Restaurant restaurant={restaurants[0]} />
+      {buttonsRestaurant}
+      <div> 
+        <Restaurant restaurant={restaurants[index]}/>
+      </div>
     </div>
   );
 };
