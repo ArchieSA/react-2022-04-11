ae370ed20ba72989a0c45777c9775c904c25aef0
brinevdev <theorion2015@gmail.com>
2022-04-19 21:44:03 +0300

    1-task

--- src/features/rate/ui/rate/component.jsx
+++ src/features/rate/ui/rate/component.jsx
@@ -1,1 +1,9 @@
-export const Rate = ({ value }) => <span>{value}</span>;
+import star from './imgs/star.svg'
+
+export const Rate = ({ value }) => {
+    const rate = [];
+    for (let i=0; i<value;i++){
+        rate.push(<img src={star}/>)
+    }
+   return <div>{rate}</div>;
+};
