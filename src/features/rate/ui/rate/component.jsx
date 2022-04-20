c3430f110c15851aa85a822d91bb7089d6c736c8
Ivan Glebov <iglebov@kameleoon.com>
2022-04-20 14:51:03 +0300

    refactoring

--- src/features/rate/ui/rate/component.jsx
+++ src/features/rate/ui/rate/component.jsx
@@ -1,10 +1,13 @@
-import Star from './img/star.svg';
-import styles from './styles.module.scss';
+import {ReactComponent as Star} from './img/star.svg';
+import './styles.scss';
 export const Rate = ({ value }) =>{
    const arr = [];
-   arr.length = value;
+   arr.length = 5;
    arr.fill(1);
-   const rate = arr.map((el, indx)=> <img  key={indx} src={Star} alt="star" className={styles.star} loading='lazy'/>)
+   const rate = arr.map((el, indx)=> 
+     <Star key={indx} 
+      className={indx<=value? 'star star--active' : 'star'}/>
+   )
      return <span> {rate} </span>;
 } 
 
