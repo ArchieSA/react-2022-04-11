8ed1e0d2975f1cd958bb8187b9a2466df4ddb0e9
Konstantin Biriukov <kostya-leon@mail.ru>
2022-04-20 09:57:19 -0500

    added Rate Stars, tabs with Restaurants, input for Rating

--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -1,9 +1,17 @@
-import { Restaurant } from "../restaurant/component";
+import {Restaurant} from "../restaurant/component";
+import {useState} from "react";
 
-export const Restaurants = ({ restaurants }) => {
-  return (
-    <div>
-      <Restaurant restaurant={restaurants[0]} />
-    </div>
-  );
+export const Restaurants = ({restaurants}) => {
+    const [currentRestaurant, setCurrentRestaurant] = useState(restaurants[0]);
+
+    return (
+        <div>
+            {restaurants.map((restaurant) => (
+                <button onClick={() => setCurrentRestaurant(restaurant)} key={restaurant.id}>
+                    {restaurant.name}
+                </button>
+            ))}
+            <Restaurant restaurant={currentRestaurant}/>
+        </div>
+    );
 };
