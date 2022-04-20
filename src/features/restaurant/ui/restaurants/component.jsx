f666f23e86fc913b39fce9915d5c2ac5d10ed6a0
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-04-20 17:35:31 +0200

    task2 part2 is completed

--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -1,9 +1,26 @@
 import { Restaurant } from "../restaurant/component";
+import styles from './styles.module.scss';
+import { useState } from "react";
 
 export const Restaurants = ({ restaurants }) => {
-  return (
-    <div>
-      <Restaurant restaurant={restaurants[0]} />
-    </div>
+
+	let [selectedRestaurant, setSelectedRestaurant] = useState(0);
+
+	return (
+		<div>
+			
+			<div className={styles.actions}>
+				{restaurants.map((restaurant, index) => (
+					<button
+						className={styles.action}
+						onClick={() => { setSelectedRestaurant(index) }}
+						key={restaurant.id}>
+						{restaurant.name}
+					</button>
+				))}
+			</div>
+
+		  <Restaurant restaurant={restaurants[selectedRestaurant]} />	
+	</div>
   );
 };
