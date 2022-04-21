6e06045f0c9e9c1ac27955ad728faa8090f5da87
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2022-04-21 12:30:40 +0300

    added the buttons

--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -1,8 +1,11 @@
 import { Restaurant } from "../restaurant/component";
 
 export const Restaurants = ({ restaurants }) => {
+  let selectedRestaurant = "";
+
   return (
     <div>
+      <button onClick={() => {selectedRestaurant=restaurants[0]}} type="button">{restaurants[0].name}</button>
       <Restaurant restaurant={restaurants[0]} />
     </div>
   );
