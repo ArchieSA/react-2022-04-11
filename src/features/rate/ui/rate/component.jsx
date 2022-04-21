ea3fb54d485db299a26ff4c7a3eb9b5fa5dcfb77
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2022-04-21 11:36:43 +0300

    Update component.jsx

--- src/features/rate/ui/rate/component.jsx
+++ src/features/rate/ui/rate/component.jsx
@@ -3,7 +3,7 @@ import Star from './imgs/star.svg';
 export const Rate = ({ value }) => {
   return (
     <div>
-      for (i = 0; i < {value}; i++) {
+      for (let i = 0; i < {value}; i++) {
         <div>{Star}</div>
       }
       <span>{value}</span>
