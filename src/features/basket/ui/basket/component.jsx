acb4b26fb54b35aa102848d4028d7e71840852c8
natallia.bahuk <natallia.bahuk@itechart-group.com>
2022-04-28 16:15:19 +0300

    HomeTask 4 from Natallia Bahuk

--- src/features/basket/ui/basket/component.jsx
+++ src/features/basket/ui/basket/component.jsx
@@ -1,10 +1,20 @@
 import classnames from "classnames";
+import React from "react";
 import styles from "./styles.module.scss";
 
-export const Basket = ({ className }) => {
+export const Basket = ({ className, products, resetBasket }) => {
   return (
     <div className={classnames(styles.root, className)}>
       <span>Basket</span>
+      {products?.map((product, index) => (
+        <div key={index} className={styles.products}>
+          <span>{product.name}</span>
+          <span>{product.amount}</span>
+        </div>
+      ))}
+      <button onClick={resetBasket} className={styles.button}>
+        Clear basket
+      </button>
     </div>
   );
 };
