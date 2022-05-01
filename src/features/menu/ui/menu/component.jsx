568996d24fa4ab7d6b851de16e2a90fa457af75a
AleKhuRu <a.khusnullin@gmail.com>
2022-05-01 20:02:59 +0300

    homework5

--- src/features/menu/ui/menu/component.jsx
+++ src/features/menu/ui/menu/component.jsx
@@ -1,16 +1,20 @@
 import classnames from "classnames";
 import styles from "./styles.module.scss";
 import { ProductContainer } from "../product/container";
+import { useSelector } from "react-redux";
+import { selectProductIds } from "../../module/selectors";
 
-export const Menu = ({ menu, className }) => (
-  <div className={classnames(styles.root, className)}>
+export const Menu = ({ menu, className }) => {
+
+  return (<div className={classnames(styles.root, className)}>
     <span className={styles.menuTitle}>Menu</span>
-    {menu.map((product) => (
-      <ProductContainer
-        key={product.id}
-        productName={product.name}
-        className={styles.product}
-      />
-    ))}
-  </div>
-);
+    {menu.length &&
+      menu.map((productId) => (
+        <ProductContainer
+          key={productId}
+          productId={productId}
+          className={styles.product}
+        />
+      ))}
+  </div>)
+};
