3a8f137efa69f17729b9bdf40a02c9ee82e5e099
Konstantin Biriukov <kostya-leon@mail.ru>
2022-05-03 00:27:23 -0500

    homework-5

--- src/features/menu/module/reducer.js
+++ src/features/menu/module/reducer.js
@@ -1,8 +1,15 @@
 import { normalizedProducts } from "../../../constants/normalized-fixtures";
 
-export const productReducer = (state = normalizedProducts, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
+const initialState = {
+    entities: normalizedProducts.reduce((acc, product) => {
+        acc[product.id] = product;
+        return acc;
+    }, {}),
+    ids: normalizedProducts.map(({id}) => id)
+}
+export const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
 };
