a1d3642e15eb40792742887c3da8d53491459ea4
Квитков Илья <kvitkovii@it2g.ru>
2022-05-04 18:58:02 +0300

    homework_5

--- src/features/menu/module/selectors.js
+++ src/features/menu/module/selectors.js
@@ -2,3 +2,6 @@ export const selectProductsModuleState = (state) => state.product;
 
 export const selectProductById = (state, payload) => 
   selectProductsModuleState(state).entities[payload];
+
+  export const selectProductIds = (state) =>
+  selectProductsModuleState(state).ids; //Я так понял тоже нужен для чего-то в будущем
