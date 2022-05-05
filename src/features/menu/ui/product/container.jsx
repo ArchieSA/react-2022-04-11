590cd5d0b1bb96c09b9a837aad0d47036d90fcd9
Ilnara Osokina <ilnara_19@mail.ru>
2022-05-05 23:24:40 +0700

    HT5

--- src/features/menu/ui/product/container.jsx
+++ src/features/menu/ui/product/container.jsx
@@ -3,23 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { useCallback } from "react";
 import { addProduct, removeProduct } from "../../../basket/module/actions";
 import { selectProductCount } from "../../../basket/module/selectors";
+import { selectProductById } from '../product/module/selectors';
 
-export const ProductContainer = ({ productName, ...props }) => {
+export const ProductContainer = ({ productId, ...props }) => {
   const dispatch = useDispatch();
-  const count = useSelector((state) => selectProductCount(state, productName));
+  const count = useSelector((state) => selectProductCount(state, productId));
+  const products = useSelector((state)=> selectProductById(state, productId));
+
 
   const decrement = useCallback(() => {
-    dispatch(removeProduct(productName));
-  }, [productName]);
+    dispatch(removeProduct(products.name));
+  }, [dispatch, products.name]);
 
   const increment = useCallback(() => {
-    dispatch(addProduct(productName));
-  }, [productName]);
+    dispatch(addProduct(products.name));
+  }, [dispatch, products.name]);
 
   return (
     <Product
       {...props}
-      productName={productName}
+      productName={products.name}
       count={count}
       decrement={decrement}
       increment={increment}
---
+++ src/features/menu/ui/product/module/reducer.js
@@ -0,0 +1,15 @@
+import { normalizedProducts } from '../../../../../constants/normalized-fixtures';
+
+const initialState = {
+  entities: normalizedProducts.reduce((acc, entity) => {
+  acc[entity.id] = entity;
+  return acc;
+}, {}), 
+ids: normalizedProducts.map(({ id })=> id)};
+
+export const productsReducer = (state=initialState, action)=> {
+  switch (action.type) {
+   default: 
+    return state;
+  }
+};
